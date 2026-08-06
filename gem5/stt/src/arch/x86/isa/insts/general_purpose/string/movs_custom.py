086a6f463939e3447a7369e2b94d8508b7d3116f
Arch-security <230239762@seu.edu.cn>
2026-08-06 20:27:58 +0800

    Align STT MOVS_ZERO macroop

--- gem5/stt/src/arch/x86/isa/insts/general_purpose/string/movs_custom.py
+++ gem5/stt/src/arch/x86/isa/insts/general_purpose/string/movs_custom.py
@@ -498,10 +498,134 @@ end:
 };
 
 def macroop MOVS_ZERO{
- # Handle RCX == 0 (no operation)
-and t0, rcx, rcx, flags=(EZF,), dataSize=asz
+
+    and t0, rcx, rcx, flags=(EZF,), dataSize=asz
+    ruflag t0, 10
+    movi t3, t3, dsz, flags=(CEZF,), dataSize=asz
+    subi t4, t0, dsz, dataSize=asz
+    mov  t3, t3, t4, flags=(nCEZF,), dataSize=asz
+
+    # Batch 1
+    ld t1, seg, [1, t0, rsi]
+    st t1, es, [1, t0, rdi]
+    add rsi, rsi, t3, dataSize=asz
+    add rdi, rdi, t3, dataSize=asz
+
+    # Batch 2
+    ld t1, seg, [1, t0, rsi]
+    st t1, es, [1, t0, rdi]
+    add rsi, rsi, t3, dataSize=asz
+    add rdi, rdi, t3, dataSize=asz
+
+    # Batch 3
+    ld t1, seg, [1, t0, rsi]
+    st t1, es, [1, t0, rdi]
+    add rsi, rsi, t3, dataSize=asz
+    add rdi, rdi, t3, dataSize=asz
+
+    # Batch 4
+    ld t1, seg, [1, t0, rsi]
+    st t1, es, [1, t0, rdi]
+    add rsi, rsi, t3, dataSize=asz
+    add rdi, rdi, t3, dataSize=asz
+
+    # Batch 5
+    ld t1, seg, [1, t0, rsi]
+    st t1, es, [1, t0, rdi]
+    add rsi, rsi, t3, dataSize=asz
+    add rdi, rdi, t3, dataSize=asz
+
+    # Batch 6
+    ld t1, seg, [1, t0, rsi]
+    st t1, es, [1, t0, rdi]
+    add rsi, rsi, t3, dataSize=asz
+    add rdi, rdi, t3, dataSize=asz
+
+    # Batch 7
+    ld t1, seg, [1, t0, rsi]
+    st t1, es, [1, t0, rdi]
+    add rsi, rsi, t3, dataSize=asz
+    add rdi, rdi, t3, dataSize=asz
+
+    # Batch 8
+    ld t1, seg, [1, t0, rsi]
+    st t1, es, [1, t0, rdi]
+    add rsi, rsi, t3, dataSize=asz
+    add rdi, rdi, t3, dataSize=asz
+
+    # Batch 9
+    ld t1, seg, [1, t0, rsi]
+    st t1, es, [1, t0, rdi]
+    add rsi, rsi, t3, dataSize=asz
+    add rdi, rdi, t3, dataSize=asz
+
+    # Batch 10
+    ld t1, seg, [1, t0, rsi]
+    st t1, es, [1, t0, rdi]
+    add rsi, rsi, t3, dataSize=asz
+    add rdi, rdi, t3, dataSize=asz
+
+    # Batch 11
+    ld t1, seg, [1, t0, rsi]
+    st t1, es, [1, t0, rdi]
+    add rsi, rsi, t3, dataSize=asz
+    add rdi, rdi, t3, dataSize=asz
+
+    # Batch 12
+    ld t1, seg, [1, t0, rsi]
+    st t1, es, [1, t0, rdi]
+    add rsi, rsi, t3, dataSize=asz
+    add rdi, rdi, t3, dataSize=asz
+
+    # Batch 13
+    ld t1, seg, [1, t0, rsi]
+    st t1, es, [1, t0, rdi]
+    add rsi, rsi, t3, dataSize=asz
+    add rdi, rdi, t3, dataSize=asz
+
+    # Batch 14
+    ld t1, seg, [1, t0, rsi]
+    st t1, es, [1, t0, rdi]
+    add rsi, rsi, t3, dataSize=asz
+    add rdi, rdi, t3, dataSize=asz
+
+    # Batch 15
+    ld t1, seg, [1, t0, rsi]
+    st t1, es, [1, t0, rdi]
+    add rsi, rsi, t3, dataSize=asz
+    add rdi, rdi, t3, dataSize=asz
+
+    # Batch 16
+    ld t1, seg, [1, t0, rsi]
+    st t1, es, [1, t0, rdi]
+    add rsi, rsi, t3, dataSize=asz
+    add rdi, rdi, t3, dataSize=asz
+
+    # Batch 17
+    ld t1, seg, [1, t0, rsi]
+    st t1, es, [1, t0, rdi]
+    add rsi, rsi, t3, dataSize=asz
+    add rdi, rdi, t3, dataSize=asz
+
+    # Batch 18
+    ld t1, seg, [1, t0, rsi]
+    st t1, es, [1, t0, rdi]
+    add rsi, rsi, t3, dataSize=asz
+    add rdi, rdi, t3, dataSize=asz
+
+    # Batch 19
+    ld t1, seg, [1, t0, rsi]
+    st t1, es, [1, t0, rdi]
+    add rsi, rsi, t3, dataSize=asz
+    add rdi, rdi, t3, dataSize=asz
+
+    # Batch 20
+    ld t1, seg, [1, t0, rsi]
+    st t1, es, [1, t0, rdi]
+    add rsi, rsi, t3, dataSize=asz
+    add rdi, rdi, t3, dataSize=asz
 end:
     fault "NoFault"
-};  
+};
 
 '''
